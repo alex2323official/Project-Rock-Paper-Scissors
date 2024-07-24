36c6040e8a2211e9348dbb16e824a315f54dac2b
Alex <alex2323official@gmail.com>
2024-07-24 13:07:07 +0200

    Finished making UI for game.

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-// 1. write a function that randomly returns “rock”, “paper” or “scissors”.
 function getComputerChoice() {
   let numberUpTo3 = Math.random();
 
@@ -11,92 +10,73 @@ function getComputerChoice() {
   }
 }
 
-// 2. write a function that takes the user choice and returns it.
-function getHumanChoice() {
-  let humanChoice = prompt("Choose rock, paper or scissors. Write it down.");
-  humanChoice = humanChoice.toLowerCase();
+let computerSelection;
+/////////////////////////////////////////////////////
+let computerScore = 0,
+  humanScore = 0;
 
-  if (
-    humanChoice != "rock" &&
-    humanChoice != "paper" &&
-    humanChoice != "scissors"
-  ) {
-    humanChoice = prompt(
-      "You wrote something wrong. Try again. Choose rock, paper or scissors."
-    );
+const rock = document.querySelector("#rock");
+const paper = document.querySelector("#paper");
+const scissors = document.querySelector("#scissors");
+
+rock.addEventListener("click", () => {
+  computerSelection = getComputerChoice();
+  playRound("rock", computerSelection);
+});
+
+paper.addEventListener("click", () => {
+  computerSelection = getComputerChoice();
+  playRound("paper", computerSelection);
+});
+
+scissors.addEventListener("click", () => {
+  computerSelection = getComputerChoice();
+  playRound("scissors", computerSelection);
+});
+/////////////////////////////////////////////////////
+
+const result = document.querySelector("#result");
+const score = document.querySelector("#score");
+
+function playRound(humanChoice, computerChoice) {
+  console.log(`${humanChoice} ${computerChoice}`);
+
+  if (humanChoice == "rock" && computerChoice == "scissors") {
+    result.textContent = "You won!";
+    humanScore++;
+    score.textContent = `SCORE. You: ${humanScore}, Computer: ${computerScore}`;
+  } else if (humanChoice == "rock" && computerChoice == "paper") {
+    result.textContent = "Computer won!";
+    computerScore++;
+    score.textContent = `SCORE. You: ${humanScore}, Computer: ${computerScore}`;
+  } else if (humanChoice == "paper" && computerChoice == "rock") {
+    result.textContent = "You won!";
+    humanScore++;
+    score.textContent = `SCORE. You: ${humanScore}, Computer: ${computerScore}`;
+  } else if (humanChoice == "paper" && computerChoice == "scissors") {
+    result.textContent = "Computer won!";
+    computerScore++;
+    score.textContent = `SCORE. You: ${humanScore}, Computer: ${computerScore}`;
+  } else if (humanChoice == "scissors" && computerChoice == "paper") {
+    result.textContent = "You won!";
+    humanScore++;
+    score.textContent = `SCORE. You: ${humanScore}, Computer: ${computerScore}`;
+  } else if (humanChoice == "scissors" && computerChoice == "rock") {
+    result.textContent = "Computer won!";
+    computerScore++;
+    score.textContent = `SCORE. You: ${humanScore}, Computer: ${computerScore}`;
+  } else {
+    result.textContent = "DRAW!";
+    score.textContent = `SCORE. You: ${humanScore}, Computer: ${computerScore}`;
   }
-  console.log(humanChoice);
-  return humanChoice;
-}
 
-// 5. write a function named playGame that calls playRound to play 5 rounds, keeps track of the scores and declares a winner at the end.
-
-function playGame() {
-  //3. write variables to keep track of the players score.
-  let humanScore = 0,
+  if (humanScore == 5) {
+    score.textContent = "!YOU WON WHOLE GAME! CONGRATULATIONS!";
+    humanScore = 0;
     computerScore = 0;
-
-  // 2. write a function that takes the user choice and returns it.
-  function getHumanChoice() {
-    let humanChoice = prompt("Choose rock, paper or scissors. Write it down.");
-    humanChoice = humanChoice.toLowerCase();
-
-    if (
-      humanChoice != "rock" &&
-      humanChoice != "paper" &&
-      humanChoice != "scissors"
-    ) {
-      humanChoice = prompt(
-        "You wrote something wrong. Try again. Choose rock, paper or scissors."
-      );
-    }
-    return humanChoice;
-  }
-
-  //   4. write a function that takes the human and computer player choices as arguments, plays a single round, increments the round winner’s score and logs a winner announcement.
-  let humanSelection = getHumanChoice();
-  let computerSelection = getComputerChoice();
-
-  function playRound(humanChoice, computerChoice) {
-    console.log(`${humanChoice} ${computerChoice}`);
-
-    if (humanChoice == "rock" && computerChoice == "scissors") {
-      humanScore++;
-    } else if (humanChoice == "rock" && computerChoice == "paper") {
-      computerScore++;
-    } else if (humanChoice == "paper" && computerChoice == "rock") {
-      humanScore++;
-    } else if (humanChoice == "paper" && computerChoice == "scissors") {
-      computerScore++;
-    } else if (humanChoice == "scissors" && computerChoice == "paper") {
-      humanScore++;
-    } else if (humanChoice == "scissors" && computerChoice == "rock") {
-      computerScore++;
-    } else {
-    }
-    humanSelection = getHumanChoice();
-    computerSelection = getComputerChoice();
-  }
-
-  playRound(humanSelection, computerSelection);
-  console.log(`Your score: ${humanScore} Computer score: ${computerScore}`);
-  playRound(humanSelection, computerSelection);
-  console.log(`Your score: ${humanScore} Computer score: ${computerScore}`);
-  playRound(humanSelection, computerSelection);
-  console.log(`Your score: ${humanScore} Computer score: ${computerScore}`);
-  playRound(humanSelection, computerSelection);
-  console.log(`Your score: ${humanScore} Computer score: ${computerScore}`);
-  playRound(humanSelection, computerSelection);
-  console.log(`Your score: ${humanScore} Computer score: ${computerScore}`);
-
-  if (humanScore > computerScore) {
-    console.log(`Player won!`);
-  } else if (humanScore < computerScore) {
-    console.log(`Computer won!`);
-  } else if (humanScore == computerScore) {
-    console.log(`Tie!`);
+  } else if (computerScore == 5) {
+    score.textContent = "!COMPUTER WON WHOLE GAME! SOLLY!";
+    humanScore = 0;
+    computerScore = 0;
   }
 }
-
-// START GAME
-playGame();
